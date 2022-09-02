94ad1d789a73c88cd8a6d41b531f2a311e907dfc
GrafKD <72695960+GrafKD@users.noreply.github.com>
2022-09-02 21:11:43 +0300

    Add files via upload

--- ns.sh
+++ ns.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
-
 apt update
 apt upgrade
-
 apt -y install mc
 apt -y install nano
-
 docker volume create portainer_data
-
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.11.1
-
 curl https://raw.githubusercontent.com/GrafKD/ns/main/docker-compose.yml --output docker-compose.yml
-
 docker compose up -d
